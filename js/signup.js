981f11ca7bb391157b6501d4c9122c55bd82ad4a
Robert <rgitonga.07@gmail.com>
2022-03-24 12:17:05 +0300

    add functionality to login form

--- js/signup.js
+++ js/signup.js
@@ -3,13 +3,25 @@ var emailAddress = document.getElementById('email');
 var password = document.getElementById('password');
 var registerUser = document.getElementById('registerUser');
 
-$(document).ready(function(){
-    $('#registerUser').click(function(e){
-        e.preventDefault();
-        localStorage.setItem('username' ,$('#email').value);
-        localStorage.setItem('password', $('password').value);
 
-        window.location.href = 'login.html'
+registerUser.addEventListener('click', registerFunc)
 
-    });
-})
+function registerFunc (e){
+    e.preventDefault();
+    localStorage.setItem('username', email.value);
+    localStorage.setItem('password', password.value);
+    window.location.href = 'login.html'
+
+
+}
+
+// $(document).ready(function(){
+//     $('#registerUser').click(function(e){
+//         e.preventDefault();
+//         localStorage.setItem('username' ,$('#email').value);
+//         localStorage.setItem('password', $('password').value);
+
+//         window.location.href = 'login.html'
+
+//     });
+// })
